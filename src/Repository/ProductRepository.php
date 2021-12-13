46c4dbbdd82e638785901ae979714a6daa2e58a4
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-12-13 16:44:37 +0100

    queryBuilder

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,6 +19,29 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
+    public function searchByTerm($term)
+    {
+        // QueryBuilder permet de créer des requêtes SQL en PHP
+        $queryBuilder = $this->createQueryBuilder('product');
+
+        // Requête
+        $query = $queryBuilder
+            ->select('product')
+            ->leftJoin('product.type', 'type') // leftJoin sur la table type
+            ->leftJoin('product.brand', 'brand') // leftJoin sur la table brand
+            ->where('product.name LIKE :term') // WHERE en SQL
+            ->orWhere('product.description LIKE :term')
+            ->orWhere('type.name LIKE :term')
+            ->orWhere('type.description LIKE :term')
+            ->orWhere('brand.name LIKE :term')
+            ->orWhere('brand.description LIKE :term')
+            ->setParameter('term', '%' . $term . '%')
+            // On attribue le term rentré et on sécurise
+            ->getQuery();
+
+        return $query->getResult();
+    }
+
     // /**
     //  * @return Product[] Returns an array of Product objects
     //  */
