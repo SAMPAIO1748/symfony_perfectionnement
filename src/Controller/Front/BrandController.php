2405b4a7fee95bc5552e0ab9ebb37824bbab802c
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-12-13 15:06:38 +0100

    front show product

---
+++ src/Controller/Front/BrandController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BrandController extends AbstractController
+{
+}
