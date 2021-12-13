3f500dd3c518abb07a6ab0c185fab2f058b266a7
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-12-13 16:26:28 +0100

    front type brand controller show list

--- src/Controller/Front/BrandController.php
+++ src/Controller/Front/BrandController.php
@@ -8,4 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BrandController extends AbstractController
 {
+
+    /**
+     * @Route("front/brands", name="front_brand_list")
+     */
+    public function brandList(BrandRepository $brandRepository)
+    {
+        $brands = $brandRepository->findAll();
+
+        return $this->render("front/brands.html.twig", ['brands' => $brands]);
+    }
+
+    /**
+     * @Route("front/brand/{id}", name="front_brand_show")
+     */
+    public function brandShow(BrandRepository $brandRepository, $id)
+    {
+        $brand = $brandRepository->find($id);
+
+        return $this->render("front/brand.html.twig", ['brand' => $brand]);
+    }
 }
--- src/Controller/Front/TypeController.php
+++ src/Controller/Front/TypeController.php
@@ -8,4 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TypeController extends AbstractController
 {
+
+    /**
+     * @Route("front/types/", name="front_type_list")
+     */
+    public function typeList(TypeRepository $typeRepository)
+    {
+        $types = $typeRepository->findAll();
+
+        return $this->render('front/types.html.twig', ['types' => $types]);
+    }
+
+    /**
+     * @Route("front/type/{id}",  name="front_type_show")
+     */
+    public function typeShow($id, TypeRepository $typeRepository)
+    {
+        $type = $typeRepository->find($id);
+
+        return $this->render('front/type.html.twig', ['type' => $type]);
+    }
 }
