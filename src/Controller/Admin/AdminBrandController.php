ff362a8e916ed5dfd565a6fce5f3c632bd5a8d17
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-12-14 17:16:07 +0100

    CRUD Brand

--- src/Controller/Admin/AdminBrandController.php
+++ src/Controller/Admin/AdminBrandController.php
@@ -2,8 +2,85 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Brand;
+use App\Form\BrandType;
+use App\Repository\BrandRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminBrandController extends AbstractController
 {
+    public function listBrand(BrandRepository $brandRepository)
+    {
+        $brands = $brandRepository->findAll();
+
+        return $this->render("admin/brands.html.twig", ['brands' => $brands]);
+    }
+
+    public function showBrand($id, BrandRepository $brandRepository)
+    {
+        $brand = $brandRepository->find($id);
+
+        return $this->render('admin/brand.html.twig', ['brand' => $brand]);
+    }
+
+    public function updateBrand(
+        $id,
+        BrandRepository $brandRepository,
+        EntityManagerInterface $entityManagerInterface,
+        Request $request
+    ) {
+
+        $brand = $brandRepository->find($id);
+
+        $brandForm = $this->createForm(BrandType::class, $brand);
+
+        $brandForm->handleRequest($request);
+
+        if ($brandForm->isSubmitted() && $brandForm->isValid()) {
+            $entityManagerInterface->persist($brand);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('admin_list_brand');
+        }
+
+
+
+        return $this->render("admin/brandform.html.twig", ['brandForm' => $brandForm->createView()]);
+    }
+
+    public function createBrand(
+        EntityManagerInterface $entityManagerInterface,
+        Request $request
+    ) {
+        $brand = new Brand();
+
+        $brandForm = $this->createForm(brandType::class, $brand);
+
+        $brandForm->handleRequest($request);
+
+        if ($brandForm->isSubmitted() && $brandForm->isValid()) {
+            $entityManagerInterface->persist($brand);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('admin_list_brand');
+        }
+
+        return $this->render("admin/brandform.html.twig", ['brandForm' => $brandForm->createView()]);
+    }
+
+    public function deleteBrand(
+        $id,
+        EntityManagerInterface $entityManagerInterface,
+        BrandRepository $brandRepository
+    ) {
+        $brand = $brandRepository->find($id);
+
+        $entityManagerInterface->remove($brand);
+
+        $entityManagerInterface->flush();
+
+        return $this->redirectToRoute('admin_list_brand');
+    }
 }
