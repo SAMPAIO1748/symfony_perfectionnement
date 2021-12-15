af7cf230a13699a367416aafc789651b96495a52
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-12-15 16:26:56 +0100

    Assert

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\ProductRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -21,6 +21,13 @@ class Product
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez remplir le nom du produit")
+     * @Assert\Length(
+     *  min = 2,
+     *  max = 20,
+     *  minMessage="Le nom doit faire minimum 2 caractères",
+     *  maxMessage="Le nom doit faire maximum 20 caractères"
+     * )
      */
     private $name;
 
